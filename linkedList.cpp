299bb6b986d96baf634569d8916eb6a337768c14
Keshav Malhotra <malhotrakeshav10@gmail.com>
2025-12-18 22:10:07 -0500

    added several more linkedList functions

--- linkedList.cpp
+++ linkedList.cpp
@@ -38,6 +38,96 @@ public:
             temp = head;
         }
     }
+    void append(int value)
+    {
+        // create a node
+        Node *newNode = new Node(value);
+        // if linkedList is empty, then make head and tail point to the newNode
+        if (length == 0)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            // have last node in linked list point to the new node
+            tail->next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    void deleteLast()
+    {
+        if (length == 0)
+            return;
+        Node *temp = head;
+        if (length == 1)
+        {
+            head = nullptr;
+            tail = nullptr;
+        }
+        else
+        {
+            Node *pre = head;
+            while (temp->next)
+            {
+                pre = temp;
+                temp = temp->next;
+            }
+            tail = pre;
+            tail->next = nullptr;
+        }
+        delete temp;
+        length--;
+    }
+    void prepend(int value)
+    {
+        Node *newNode = new Node(value);
+        if (length == 0)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+    void deleteFirst()
+    {
+        Node *temp = head;
+        if (length == 0)
+            return;
+        else if (length == 1)
+        {
+            head = nullptr;
+            tail = nullptr;
+        }
+        else
+        {
+            head = head->next;
+        }
+        delete temp;
+        length--;
+    }
+    Node *get(int index)
+    {
+        Node *temp = head;
+        if (index < 0 || index >= length)
+            return nullptr;
+        else
+        {
+            for (int i = 0; i < index; i++)
+            {
+                temp = temp->next;
+            }
+            return temp;
+        }
+    }
 
     void printList()
     {
@@ -67,9 +157,8 @@ public:
 
 int main()
 {
-    LinkedList *myLinkedList = new LinkedList(4);
-    myLinkedList->getHead();
-    myLinkedList->getTail();
-    myLinkedList->getLength();
-    myLinkedList->printList();
+    LinkedList *myLinkedList = new LinkedList(1);
+    myLinkedList->append(2);
+    myLinkedList->append(3);
+    cout << myLinkedList->get(2)->value;
 }
