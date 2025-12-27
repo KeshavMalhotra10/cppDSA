4e28f57f878b559d0efb32eecc818906f91f490c
Keshav Malhotra <malhotrakeshav10@gmail.com>
2025-12-27 10:40:39 -0500

    fixed all bugs in this leetcode excercise

--- hash/findPairs.cpp
+++ hash/findPairs.cpp
@@ -1,3 +1,13 @@
+//   +=========================================================+
+//   | Description:                                            |
+//   | - This function finds all pairs of integers from        |
+//   |   'arr1' and 'arr2' that sum up to 'target'.            |
+//   | - It uses an unordered_set for quick look-up.           |
+//   | - It returns a 2D vector containing the pairs.          |
+//   |                                                         |
+//   | Return type: vector<vector<int>>                        |
+//   +=========================================================+
+
 #include <iostream>
 #include <vector>
 #include <unordered_set>
@@ -6,42 +16,23 @@ using namespace std;
 
 vector<vector<int>> findPairs(const vector<int> &arr1, const vector<int> &arr2, int target)
 {
-    unordered_set<int> mySet1;
-    vector<vector<int>> resultantVector;
+    unordered_set<int> mySet;
+    vector<vector<int>> pairs;
+
+    // populate mySet
     for (auto i : arr1)
     {
-        mySet1.insert(i);
+        mySet.insert(i);
     }
-    unordered_set<int> mySet2;
+
+    // loop through arr2 to find pairs
     for (auto i : arr2)
     {
-        mySet2.insert(i);
-    }
-    for (auto i : mySet1)
-    {
         int needed = target - i;
-        if (mySet2.find(needed) != mySet2.end())
+        if (mySet.find(needed) != mySet.end())
         {
-            resultantVector.push_back({i, needed});
+            pairs.push_back({i, needed});
         }
     }
-    return resultantVector;
-    //   +=========================================================+
-    //   |                 WRITE YOUR CODE HERE                    |
-    //   | Description:                                            |
-    //   | - This function finds all pairs of integers from        |
-    //   |   'arr1' and 'arr2' that sum up to 'target'.            |
-    //   | - It uses an unordered_set for quick look-up.           |
-    //   | - It returns a 2D vector containing the pairs.          |
-    //   |                                                         |
-    //   | Return type: vector<vector<int>>                        |
-    //   |                                                         |
-    //   | Tips:                                                   |
-    //   | - 'mySet' stores numbers from 'arr1'.                   |
-    //   | - 'pairs' will store the pairs that meet the target.    |
-    //   | - Loop through 'arr1' to populate 'mySet'.              |
-    //   | - Loop through 'arr2' to find pairs.                    |
-    //   | - Use 'find' method for quick look-up in set.           |
-    //   | - Check output from Test.cpp in "User logs".            |
-    //   +=========================================================+
+    return pairs;
 }
